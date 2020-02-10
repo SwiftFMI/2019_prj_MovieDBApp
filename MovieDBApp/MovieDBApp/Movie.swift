08e999f1fe16501250682ea7cd69a3bde90c8486
Damyan Boychev <dboychev@astea.net>
2020-02-10 15:44:58 +0200

    Add Initial MVC declarations

---
+++ MovieDBApp/MovieDBApp/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  MovieDBApp
+//
+//  Created by User on 7.02.20.
+//  Copyright © 2020 TwoGirlsOneApp. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    
+}
