2c303aec30a009d3134fa278ad22155c9b8a7792
Damyan Boychev <dboychev@astea.net>
2020-02-10 20:16:06 +0200

    Add Movie Service

--- MovieDBApp/MovieDBApp/MovieService.swift
+++ MovieDBApp/MovieDBApp/MovieService.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class MovieService {
+    
+    func getMovies(page: Int, completionHandler: (([Movie]) -> Void)) {
+        var urlComponents = URLComponents(url: Constants.DISCOVER_URL)!
+        
+        let apiKeyQueryItem = URLQueryItem(name: "api_key", value: Constants.API_KEY)
+        let languageQueryItem = URLQueryItem(name: "language", value: "en-US")
+        let sortByQueryItem = URLQueryItem(name: "sort_by", value: "popularity.desc")
+        let includeAdultQueryItem = URLQueryItem(name: "include_adult", value: "false")
+        let includeVideoQueryItem = URLQueryItem(name: "include_video", value: "false")
+        let pageQueryItem = URLQueryItem(name: "page", value: "\(page)")
+        
+        urlComponents.queryItems = [apiKeyQueryItem, languageQueryItem, sortByQueryItem, includeAdultQueryItem, includeVideoQueryItem, pageQueryItem]
+        
+        URLSession.shared.dataTask(with: urlComponents.url!) { data,response,error in
+            if let data = data {
+                print(String(data: data, encoding: .utf8))
+            }
+        }.resume()
+    }
+}
+
