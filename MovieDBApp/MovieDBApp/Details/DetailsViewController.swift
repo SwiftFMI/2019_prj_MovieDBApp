9d13bf52c8e5fc3c987e83800cdea7b4ea7432e0
Damyan Boychev <dboychev@astea.net>
2020-02-16 13:12:47 +0200

    Add test movie poster

---
+++ MovieDBApp/MovieDBApp/Details/DetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsViewController.swift
+//  MovieDBApp
+//
+//  Created by User on 16.02.20.
+//  Copyright © 2020 TwoGirlsOneApp. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
